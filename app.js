3c1f279d4b67f9b7affbad6fc9e6234853988bcb
Mehdi <alialipoor1985@yahoo.com>
2019-08-30 19:06:57 +0400

    ssr react + client react >> index

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const React = require("react")
 const ReactDOMServer = require("react-dom/server")
-const path = require('path');
-const logger = require('morgan');
-const helmet = require('helmet')
-const favicon = require('serve-favicon');
-const createError = require('http-errors');
-const indexRouter = require('./src/server/routes/index');
-const usersRouter = require('./src/server/routes/users');
-var port = process.env.PORT || '3070'
-
+const path = require("path")
+const logger = require("morgan")
+const helmet = require("helmet")
+const favicon = require("serve-favicon")
+const createError = require("http-errors")
+const indexRouter = require("./src/server/routes/index")
+const usersRouter = require("./src/server/routes/users")
+var port = process.env.PORT || "3070"
 
 // app.use(logger('dev'));
 // app.use(helmet({
@@ -22,41 +21,35 @@ var port = process.env.PORT || '3070'
 // 		scriptSrc: ["'self'", 'www.google-analytics.com', 'ajax.googleapis.com', 'www.googletagmanager.com' ]
 // 	}
 // }))
-app.use("/public", express.static(path.join(__dirname , "public") ))
-app.use(favicon(path.join(__dirname, 'public', "images", 'favicon.ico')))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+// app.use("/build", express.static(path.join(__dirname , "build") ))
+app.use(express.static(path.join(__dirname, "build")))
+// app.use("/public", express.static(path.join(__dirname , "public") ))
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")))
 
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
 //routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404));
-});
+// app.use(function(req, res, next) {
+// 	next(createError(404))
+// })
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get("env") === "development" ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-
-
-
-
-
-
+	// render the error page
+	res.status(err.status || 500)
+	res.render("error")
+})
 
 app.listen(port, console.log(`skytours-node app is listening on ${port}`))
-module.exports = app;
+module.exports = app
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,22 +1,36 @@
-const express = require('express');
-const app = express.Router();
+const path = require("path")
+const express = require("express")
+const app = express.Router()
 const babel = require("babel-register")
 const React = require("react")
 const ReactDOMServer = require("react-dom/server")
 
-require('babel-register')({
-  presets: [ 'react' ]
+require("babel-register")({
+	presets: ["react"]
 })
 
-const Component =  require("../../client/component.js") 
+const Component = require("../../client/component.js")
 
-app.get('/', function(req, res, next) {
-	 const reactHTML = ReactDOMServer.renderToString(Component())
-console.log(reactHTML)
-	res.render('index', { 
-		title: 'Express', 
-		react: reactHTML 
-	});
-});
+app.get("/", function(req, res, next) {
+	const reactHTML = ReactDOMServer.renderToString(Component())
+	console.log(reactHTML)
+	res.render(path.join(__dirname, "..", "..", "..", "build", "index.ejs"), {
+		title: "Express",
+		react: reactHTML
+	})
+})
 
-module.exports = app;
+// app.get("/", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "..", "..", "..", "build", "index.html"))
+// })
+
+module.exports = app
+
+/*
+sky-tours.com >> React app  ???
+
+static pages: about us
+
+
+
+*/
