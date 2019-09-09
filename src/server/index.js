be26f781778b4a403337b3c474cebca304081e3f
Mehdi <alialipoor1985@yahoo.com>
2019-09-09 19:57:45 +0400

    footer titles translations

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
-const path = require("path")
+const { join } = require("path")
 const express = require("express")
 const app = express.Router()
+const { readContent } = require("../../functions") //pass paths as if you are in root folder
 // const babel = require("babel-register")
 // const React = require("react")
 // const ReactDOMServer = require("react-dom/server")
@@ -11,21 +12,23 @@ const app = express.Router()
 
 // const Component = require("../../client/index.js")
 
-app.get("*", (req, res, next) => { 
-	let lang = ""
-	if (/^\/{0,1}[A-Za-z]{2}\/{0,1}$/.test(req.params["0"])) {
-		lang = req.params["0"].match(/\w{2}/)[0]
-	}
-	// const reactHTML = ReactDOMServer.renderToString(Component())
-	res.render("index", {
-		// react: reactHTML,
-		minHeight: "700px",
-		lang: lang,
-		//if there is a variable defined in ejs, it must be supplied, although with null:
-		static: null,
-		custom: null
-	})
+app.get("*", (req, res, next) => {
+	const lang = req.baseUrl.split("/")[1] || "en"
 
+	readContent(join("build", "locales", "lang", lang + ".json"))
+		.then(json => JSON.parse(json))
+		.then(translationsObject => {
+			// const reactHTML = ReactDOMServer.renderToString(Component())
+			res.render("index", {
+				// react: reactHTML,
+				minHeight: "700px",
+				lang: lang,
+				t: word => translationsObject[word.trim()],
+				//if there is a variable defined in ejs, it must be supplied, although with null:
+				static: null,
+				custom: null
+			})
+		})
 })
 
 module.exports = app
