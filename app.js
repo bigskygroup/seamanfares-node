c9123b4e2e9dd91132dd457b7b95016c48a82dab
Mehdi <alialipoor1985@yahoo.com>
2019-09-12 16:56:32 +0400

    completing all react routes, /book, /confirmation , etc

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(favicon(join(__dirname, "build", "favicon.ico")))
 app.set("views", join(__dirname, "build"))
 app.set("view engine", "ejs")
 
-createIndexEJS(join(__dirname, "build"))
+// createIndexEJS(join(__dirname, "build"))
 
 const airportsArray = memoize(extractToRegex(airports))
 
@@ -37,11 +37,23 @@ const airportsArray = memoize(extractToRegex(airports))
 app.use(airportsArray("code"), require("./src/server/seo-city"))
 app.use(airportsArray("cc"), require("./src/server/seo-country"))
 app.use(/^\/[A-Za-z]{2}\/[A-Za-z_]{2,22}\.htm\/{0,1}$/, require("./src/server/static"))
-app.use([/^\/$/, /^\/[A-Za-z]{2}\/{0,1}$/], require("./src/server/index"))
+
+app.get("/advertising.html", (req, res) => res.render("pages/advertising.ejs"))
+app.get(["/book", "/confirmation"] , require("./src/server/index"))
+
+const routeToIndex = [
+/^\/[A-Za-z]{2}\/confirmation/,
+/^\/[A-Za-z]{2}\/search\/{0,1}/,
+/^\/[A-Za-z]{2}\/{0,1}$/, /^\/$/]
+
+app.use( routeToIndex, require("./src/server/index"))
+
+app.get("/book" , require("./src/server/index"))
 
 //handeling wrong requests at the end
 app.use((req, res) => {
-	// console.log(req.originalUrl)
+
+	console.log(req.originalUrl)
 	res.status(404)
 	res.send("<h1 style='width:50%; margin: 20px 20px;'>No page found for your request! </h1>")
 })
