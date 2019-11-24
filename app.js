d79373ffe8872d53cc210e8ca310a1f006b1ce64
Mehdi <alialipoor1985@yahoo.com>
2019-11-24 17:08:44 +0400

    test write file

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ mongoose
 	.catch(err => console.log("Your MongoDB setting in the app.js file is not correct. ", err))
 const IP = require("./src/server/models/ip")
 
+fs.writeFileSync("./testingIfItWrites", "some data")
 // const myIp = new IP({ name: new Date().getSeconds().toString() })
 // myIp
 // 	.save()
