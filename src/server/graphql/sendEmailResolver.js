fea20f1255dd87f3c96a7bd3f16c279a13a62f19
Mehdi <alialipoor1985@yahoo.com>
2019-12-13 11:21:35 +0400

    sendEmailResolver

--- src/server/graphql/sendEmailResolver.js
+++ src/server/graphql/sendEmailResolver.js
@@ -9,9 +9,10 @@ const r = {}
 // const json = require("../../client/viewtrip")
 
 r.sendEmail = async ({ json }) => {
+	// console.log(json)
 	// console.log(JSON.parse(json))
 	//incoming customer information, from react, after ticket purchase, for sending email and storing in db:
-	const { customer_email, customer_name, order, lname, gross_price, gross_cur } = JSON.parse(json)
+	const { customer_email, customer_name, order, lname, gross_price, gross_curr } = JSON.parse(json)
 	const customerInformation = {
 		data: json, // where all the information is stored as json
 		customer_email: customer_email || "",
@@ -19,7 +20,7 @@ r.sendEmail = async ({ json }) => {
 		order: order || "123",
 		lname: lname || "",
 		gross_price: gross_price || "",
-		gross_cur: gross_cur || ""
+		gross_curr: gross_curr || ""
 	}
 
 	//error handling:
@@ -45,13 +46,14 @@ r.sendEmail = async ({ json }) => {
 	const sending = await sendEmail(customerInformation)
 
 	return {
-			id: order,
-			json: json,
-			error: error
-		}
+		id: order,
+		json: json,
+		error: error
+	}
 }
 
-async function sendEmail(obj) { console.log("obj:" , obj)
+async function sendEmail(obj) {
+	//console.log("obj:" , obj)
 	const nameFrom = "Sky-tours.com <support@sky-tours.com>"
 	const emailTo = obj.customer_email
 	const emailCc = ""
@@ -61,39 +63,47 @@ async function sendEmail(obj) { console.log("obj:" , obj)
 	const confirmationId = obj.order
 	// all fields are strings
 
-	const emailMessageHtml = await readContent(join("src", "client", "confirmationEmail.html"), "utf8").then(data => {
-		const emailContent = ejs(data)(JSON.parse(obj.data))
-		return emailContent
-	})
+	const emailMessageHtml = await readContent(join("src", "client", "confirmationEmail.html"), "utf8").then(
+		data => {
+			const emailContent = ejs(data)(JSON.parse(obj.data))
+			return emailContent
+		}
+	)
 
-console.log(emailMessageHtml)
+	// console.log(emailMessageHtml)
+
 	const data = {
 		nameFrom: nameFrom,
 		emailTo: emailTo,
 		emailCc: emailCc,
-		// emailBcc: emailBcc,
+		emailBcc: emailBcc,
 		emailSubject: emailSubject,
-		emailMessageTxt,
-		emailMessageHtml,
-		emailConfirmId: order
+		emailMessageTxt: emailMessageTxt,
+		emailMessageHtml: emailMessageHtml,
+		emailConfirmId: obj.order
 	}
-	console.log("email request recieved")
 
-	// await fetch("https://email.hotelshop.com/sendemail", {
-	// 	method: "post",
-	// 	body: JSON.stringify(data),
-	// 	headers: { "Content-Type": "application/json" }
-	// })
-	// 	.then(res => {
-	// 		if (res.status == 200)
-	// 			console.log(`✔ confirmation email sent to ${emailTo} at ${new Date().toLocaleString()}`)
-	// 		else {
-	// 			console.error(`✘ confirmation email fail to ${emailTo} at ${new Date().toLocaleString()}`)
-	// 		}
-	// 	})
-	// 	.catch(err => console.error("✘ err was: ", err, new Date().toLocaleString()))
+	// console.log("email request recieved" , data)
 
+	await fetch("https://email.hotelshop.com/sendemail", {
+		method: "post",
+		body: JSON.stringify(data),
+		headers: { "Content-Type": "application/json" }
+	})
+		.then(res => {
+			if (res.status == 200) {
+				console.log(`✔ confirmation email sent to ${emailTo} at ${new Date().toLocaleString()}`)
 
+				// set the emailSent field true in database
+				Customer.updateOne({ order: obj.order }, { emailSent: true })
+					.then(res => res)
+					.catch(err => console.error(err, `cannot update emailSent to true for order ${obj.order}`))
+			} else {
+				console.error(`✘ confirmation email fail to ${emailTo} at ${new Date().toLocaleString()}`)
+			}
+		})
+		.catch(err => console.error("✘ err was: ", err, new Date().toLocaleString()))
 }
 
 module.exports = r
+
