845be32afb78a3a443635928c55e1bd775ee8ed3
Mehdi <alialipoor1985@yahoo.com>
2019-12-31 14:41:14 +0400

    test4

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ const routeToIndex = [
 	/^\/[A-Za-z]{2}\/404$/,
 	/^\/$/
 ]
-console.log("testing repo update 3")
+console.log("testing repo update 4")
 
 app.use(routeToIndex, require("./src/server/index"))
 
