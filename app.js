0ff4a2fc9aabcdfc6b923cdf43ebf61ba3d534ee
Mehdi <alialipoor1985@yahoo.com>
2020-01-08 15:22:28 +0400

    test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const airports = require("./data/cities-condensed")
 const countries = require("./data/countries")
 const { dbName, dbPassword, dbAccessIP, NODE_ENV } = require("./config.js")
 const PORT = process.env.PORT || 3070
-process.env.NODE_ENV = NODE_ENV
+process.env.NODE_ENV = NODE_ENV 
 
 //database connection
 mongoose
