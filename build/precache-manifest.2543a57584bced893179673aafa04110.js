e9282ba051d7beb0f19e5ba64529ccae45a67832
Mehdi <alialipoor1985@yahoo.com>
2020-02-20 14:22:19 +0400

    update react

---
+++ build/precache-manifest.2543a57584bced893179673aafa04110.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "acddc9cf06a6dc5729ab",
+    "url": "/static/js/6.2680d1f9.chunk.js"
+  },
+  {
+    "revision": "66b86802679d0d68b421",
+    "url": "/static/js/0.0deb6181.chunk.js"
+  },
+  {
+    "revision": "e5f6f00efcb60e1e6b4b",
+    "url": "/static/js/14.a21cfd62.chunk.js"
+  },
+  {
+    "revision": "477f471212d453b59211",
+    "url": "/static/js/2.e643f845.chunk.js"
+  },
+  {
+    "revision": "406d1024b5a3db02c746",
+    "url": "/static/js/13.d678d97d.chunk.js"
+  },
+  {
+    "revision": "8cdaf93300342308b243",
+    "url": "/static/js/main.8c113665.chunk.js"
+  },
+  {
+    "revision": "1855a3e9df11aa5f6c6d",
+    "url": "/static/js/runtime~main.6c33862b.js"
+  },
+  {
+    "revision": "0e33f29faa24cb77dd48",
+    "url": "/static/js/12.ae6d7b67.chunk.js"
+  },
+  {
+    "revision": "e63cf33f309ed8bdf089",
+    "url": "/static/js/5.b9525b85.chunk.js"
+  },
+  {
+    "revision": "9429d83c8c0c97a0afa5",
+    "url": "/static/js/11.4348e3a1.chunk.js"
+  },
+  {
+    "revision": "204bfe76b86f2a4b4b05",
+    "url": "/static/js/1.f45f481c.chunk.js"
+  },
+  {
+    "revision": "bbbf926b914a81916864",
+    "url": "/static/js/7.84c63c32.chunk.js"
+  },
+  {
+    "revision": "a6a4ae3bd1ef0052e045",
+    "url": "/static/js/8.a6e628ec.chunk.js"
+  },
+  {
+    "revision": "7a6051d9b69f8120f449",
+    "url": "/static/js/9.40b68bd9.chunk.js"
+  },
+  {
+    "revision": "221c0196587f096c1f3d",
+    "url": "/static/js/10.f06c5d0a.chunk.js"
+  },
+  {
+    "revision": "8cdaf93300342308b243",
+    "url": "/static/css/main.a2b3c1c5.chunk.css"
+  },
+  {
+    "revision": "acddc9cf06a6dc5729ab",
+    "url": "/static/css/6.02b479bb.chunk.css"
+  },
+  {
+    "revision": "e63cf33f309ed8bdf089",
+    "url": "/static/css/5.4eb7d6b4.chunk.css"
+  },
+  {
+    "revision": "477f471212d453b59211",
+    "url": "/static/css/2.bedf034f.chunk.css"
+  },
+  {
+    "revision": "a48005b7d537c9ac0432f77e2578ff5a",
+    "url": "/index.html"
+  }
+];
