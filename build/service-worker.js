dcb435ff6f01512623b0308000a0a6dbb2d1c3c1
Mehdi <alialipoor1985@yahoo.com>
2020-03-26 13:28:52 +0400

    update react

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95126f75c3b66b07e2d3cddf62159a19.js"
+  "/precache-manifest.e9458b94b6d62705919ad764e2a5adb7.js"
 );
 
 workbox.clientsClaim();
