16897197bc7947cb0da1cc767938b1affa7dc35c
Mehdi <alialipoor1985@yahoo.com>
2020-03-30 14:24:16 +0400

    emailApp api created

--- src/server/graphql/resolvers.js
+++ src/server/graphql/resolvers.js
@@ -3,5 +3,6 @@ const logResolver = require("./logResolver")
 const sendEmailResolver = require("./sendEmailResolver")
 const decryptResolver = require("./decryptResolver")
 const confhtmlResolver = require("./confhtmlResolver")
+const emailAppResolver = require("./emailAppResolver")
 
-module.exports = Object.assign({}, ipResolver , logResolver, sendEmailResolver, decryptResolver, confhtmlResolver)
+module.exports = Object.assign({}, ipResolver , logResolver, sendEmailResolver, decryptResolver, confhtmlResolver, emailAppResolver)
