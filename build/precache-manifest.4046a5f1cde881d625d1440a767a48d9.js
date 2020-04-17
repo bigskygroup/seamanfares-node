a2c01b69c41d9b2b9716bd64982ae1dea450584a
Mehdi <alialipoor1985@yahoo.com>
2020-04-17 12:31:02 +0400

    update react

---
+++ build/precache-manifest.4046a5f1cde881d625d1440a767a48d9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ecc45670430f63063d",
+    "url": "/static/js/7.cbc8db6b.chunk.js"
+  },
+  {
+    "revision": "4f04c3d72b4165c583a1",
+    "url": "/static/js/0.98d882af.chunk.js"
+  },
+  {
+    "revision": "5d0fbb5b46d8314c7b9d",
+    "url": "/static/js/16.88bfd064.chunk.js"
+  },
+  {
+    "revision": "520c22d1c9c52c00dfba",
+    "url": "/static/js/2.0009b6a2.chunk.js"
+  },
+  {
+    "revision": "cc28e2342ecd28b3ccf3",
+    "url": "/static/js/3.454d3bbd.chunk.js"
+  },
+  {
+    "revision": "7fc65a4f4083ccfe8c85",
+    "url": "/static/js/15.1babe8af.chunk.js"
+  },
+  {
+    "revision": "cabda0c407263d1b6134",
+    "url": "/static/js/main.82944ab6.chunk.js"
+  },
+  {
+    "revision": "6bf5cc8ab4ad30a27e5b",
+    "url": "/static/js/runtime~main.7bba177c.js"
+  },
+  {
+    "revision": "cb8fc55da8cde094d835",
+    "url": "/static/js/14.639d1975.chunk.js"
+  },
+  {
+    "revision": "83a1d3d591ede2fdf8bd",
+    "url": "/static/js/6.bb674d55.chunk.js"
+  },
+  {
+    "revision": "b62d1420ff8472aba95b",
+    "url": "/static/js/13.72bf2b14.chunk.js"
+  },
+  {
+    "revision": "c533e7550ae5149c4b4d",
+    "url": "/static/js/1.4c1ad89a.chunk.js"
+  },
+  {
+    "revision": "406fa353dea6fff8c694",
+    "url": "/static/js/8.96c707f9.chunk.js"
+  },
+  {
+    "revision": "fc140b23643bc0177bf0",
+    "url": "/static/js/9.a3c5ddf0.chunk.js"
+  },
+  {
+    "revision": "d53a2b1d01d754850860",
+    "url": "/static/js/10.1ef29ea3.chunk.js"
+  },
+  {
+    "revision": "2ac8625200663b66a78a",
+    "url": "/static/js/11.a9144e6e.chunk.js"
+  },
+  {
+    "revision": "7dee30e6364bcabc2ebc",
+    "url": "/static/js/12.f76ac18f.chunk.js"
+  },
+  {
+    "revision": "cabda0c407263d1b6134",
+    "url": "/static/css/main.b2bd9c82.chunk.css"
+  },
+  {
+    "revision": "e4ecc45670430f63063d",
+    "url": "/static/css/7.02b479bb.chunk.css"
+  },
+  {
+    "revision": "83a1d3d591ede2fdf8bd",
+    "url": "/static/css/6.4eb7d6b4.chunk.css"
+  },
+  {
+    "revision": "520c22d1c9c52c00dfba",
+    "url": "/static/css/2.bedf034f.chunk.css"
+  },
+  {
+    "revision": "b62d1420ff8472aba95b",
+    "url": "/static/css/13.2a0c79e7.chunk.css"
+  },
+  {
+    "revision": "8d89326eea309d5a891b18519df728e9",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d501443169daabcd21bd055da9e2d2f4.js"
+  "/precache-manifest.4046a5f1cde881d625d1440a767a48d9.js"
 );
 
 workbox.clientsClaim();
