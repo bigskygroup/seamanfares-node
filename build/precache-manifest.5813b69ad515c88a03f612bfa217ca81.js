d123a4122bc75acee5219eb58183b1e33464ecd6
Mehdi <alialipoor1985@yahoo.com>
2020-05-14 09:14:08 +0400

    update react

--- build/precache-manifest.5813b69ad515c88a03f612bfa217ca81.js
+++ build/precache-manifest.5813b69ad515c88a03f612bfa217ca81.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "820af7277817c8065c84",
-    "url": "/static/js/8.0149cb3f.chunk.js"
+    "revision": "5d5de5d0580be133292a",
+    "url": "/static/js/8.938f4bcd.chunk.js"
   },
   {
     "revision": "fa470749d164dc94a005",
     "url": "/static/js/0.8df4ba20.chunk.js"
   },
   {
-    "revision": "a219b0fc064721ef4c7d",
-    "url": "/static/js/2.ab8d9441.chunk.js"
+    "revision": "841e2cbf0f4673b96884",
+    "url": "/static/js/2.b64590a7.chunk.js"
   },
   {
     "revision": "9e17a6578ffe7e40fe67",
     "url": "/static/js/19.15098e41.chunk.js"
   },
   {
-    "revision": "3e9c605d298525048821",
-    "url": "/static/js/3.fd28a6ba.chunk.js"
+    "revision": "29eba05c2adcff4af95f",
+    "url": "/static/js/3.452d8a0e.chunk.js"
   },
   {
     "revision": "17cfcce2c39421a02623",
@@ -32,8 +32,8 @@ self.__precacheManifest = [
     "url": "/static/js/main.895f4d3b.chunk.js"
   },
   {
-    "revision": "03f542d476216c11bba5",
-    "url": "/static/js/runtime~main.df7cd5b4.js"
+    "revision": "64da04f8176e4f4bbb6c",
+    "url": "/static/js/runtime~main.d3ca709b.js"
   },
   {
     "revision": "756abc7932a46d2c994d",
@@ -48,24 +48,24 @@ self.__precacheManifest = [
     "url": "/static/js/16.9fe43e21.chunk.js"
   },
   {
-    "revision": "4482142945e46d2635f5",
-    "url": "/static/js/1.24075a18.chunk.js"
+    "revision": "e1c9f216fa5eee36d1b9",
+    "url": "/static/js/1.99ef7064.chunk.js"
   },
   {
-    "revision": "1fb4db764f860a43811b",
-    "url": "/static/js/9.14d62ae1.chunk.js"
+    "revision": "3440e0a67ba86d13ea39",
+    "url": "/static/js/9.f55e5f46.chunk.js"
   },
   {
-    "revision": "a3e342c28f5920448a51",
-    "url": "/static/js/10.eb5ef026.chunk.js"
+    "revision": "6c952413dff2470fa818",
+    "url": "/static/js/10.c49f4fe4.chunk.js"
   },
   {
-    "revision": "b4429e3f6df83fdb62b9",
-    "url": "/static/js/11.563fb155.chunk.js"
+    "revision": "046fb37bd9135dfc1ccf",
+    "url": "/static/js/11.c544bbad.chunk.js"
   },
   {
-    "revision": "1d04d02f55d549445539",
-    "url": "/static/js/12.85feda64.chunk.js"
+    "revision": "cb147a1f59ca3c4f6e7b",
+    "url": "/static/js/12.f93143fd.chunk.js"
   },
   {
     "revision": "1f96c6ca65bbe82537e8",
@@ -76,15 +76,15 @@ self.__precacheManifest = [
     "url": "/static/js/14.f739209f.chunk.js"
   },
   {
-    "revision": "a90084decdff003ef6e1",
-    "url": "/static/js/15.fd034d91.chunk.js"
+    "revision": "442f0b828e6afc1bc9fb",
+    "url": "/static/js/15.bbb6048f.chunk.js"
   },
   {
     "revision": "18c35f211d5bf51bafe7",
     "url": "/static/css/main.888eed8f.chunk.css"
   },
   {
-    "revision": "820af7277817c8065c84",
+    "revision": "5d5de5d0580be133292a",
     "url": "/static/css/8.02b479bb.chunk.css"
   },
   {
@@ -92,11 +92,11 @@ self.__precacheManifest = [
     "url": "/static/css/7.4eb7d6b4.chunk.css"
   },
   {
-    "revision": "3e9c605d298525048821",
+    "revision": "29eba05c2adcff4af95f",
     "url": "/static/css/3.bedf034f.chunk.css"
   },
   {
-    "revision": "7ee98e34714510f870be6e569f5a5978",
+    "revision": "520937d3ef4e5461ffa764dbf71fa5fa",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1eb1661f5c26fed98be4d20be3e866bb.js"
+  "/precache-manifest.5813b69ad515c88a03f612bfa217ca81.js"
 );
 
 workbox.clientsClaim();
